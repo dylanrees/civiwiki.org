c7a0b2e9f35d3a4f220e2b794a2184dbf9b2f4b6
Darius Calliet <dcalliet@topoppssoftware.com>
2016-01-30 17:12:51 -0600

    ayeee static files

--- civiwiki/civiwiki/settings.py
+++ civiwiki/civiwiki/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False #Eh deal with timezones on front-end, I dislike them
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/frontend/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "../frontend/static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
