455447b968a8558213ad2402787577b3845b5f10
Darius Calliet <dcalliet@topoppssoftware.com>
2016-02-03 23:34:44 -0600

    login flow and model change

--- civiwiki/api/login_views.py
+++ civiwiki/api/login_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from models import Account
 
 def login(request):
@@ -6,14 +7,30 @@ def login(request):
 	'''
 	username = request.POST.get('username', '')
 	password = request.POST.get('password', '')
-	account = {}
-	try:
-		account = Account.objects.get(username=username, password=password)
-	except Exception as e:
-		return JsonResponse({'status': 400, 'result': 'User not found'})
-
-	key = account.secret_key
-	return JsonResponse({'status_code': 200, 'result': key})
+	user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            return JsonResponse({'status_code': 400, 'error': 'inactive account'})
+    else:
+        # Return an 'invalid login' error message.
+		return JsonResponse({'status_code': 400, 'error': 'invalid username / password combination'})
 
 def register(request):
-    return JsonResponse({'status_code': 500, 'error': 'under construction'})
+	username = request.POST.get('username', '')
+	password = request.POST.get('password', '')
+	email = request.POST.get('email', '')
+	first_name = request.POST.get('first_name', '')
+	last_name = request.POST.get('last_name', '')
+	try:
+		user = User.objects.create_user(username, email, password)
+		account = Account(user=user, email=email, first_name=first_name, last_name=last_name)
+		account.save()
+
+		login(request, user)
+	    return JsonResponse({'status_code': 200})
+	except:
+		return JsonResponse({'status_code': 500})
