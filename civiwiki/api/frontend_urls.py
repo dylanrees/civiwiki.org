ea4d700609570233e94b0699cfded78c93dd35ea
Grace Lu <lu.grace.wy@gmail.com>
2016-02-05 20:50:48 -0500

    added urls/views to see supportus page on server

--- civiwiki/api/frontend_urls.py
+++ civiwiki/api/frontend_urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 import template_views as views
 
 urlpatterns = [
-    url(r'^foo$', views.hello_view, name='foo')
+    url(r'^foo$', views.hello_view, name='foo'),
+    url(r'^supportus$', views.support_us_view, name='supportus')
 ]
