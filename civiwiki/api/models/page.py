ca0f6836804075554a085f029fe5da11b4bf3740
Darius Calliet <dcalliet@topoppssoftware.com>
2016-02-15 22:10:18 -0600

    pages errors fixed

--- civiwiki/api/models/page.py
+++ civiwiki/api/models/page.py
@@ -4,11 +4,11 @@ import datetime
 
 # Create your models here.
 class Page(models.Model):
-    owner = models.ForeignKey('Account')
+    owner = models.ForeignKey('Account', related_name='page_owner')
     title = models.CharField(max_length=63, default='')
     body = models.TextField(max_length=4095)
     profile_image = models.CharField(max_length=255)
     cover_image = models.CharField(max_length=255)
-    contributors = models.
+    contributors = models.ManyToManyField('Account')
     def retrieve(self, user):
         return self.find(user=user)[0]
