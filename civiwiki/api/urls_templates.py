c01fecd4aed3a169fc609064c84c0ac0cdba1e7d
LawranceFung <fungjl@wustl.edu>
2016-02-17 17:01:46 -0600

    Second, still Alpha

--- civiwiki/api/urls_templates.py
+++ civiwiki/api/urls_templates.py
@@ -3,7 +3,7 @@ import views_templates as v
 
 urlpatterns = [
 	url(r'^privacy_policy$', v.privacyPolicy, name='privacy_policy'),
-    url(r'^privacy_policy$', v.userAgreement, name='privacy_policy'),
+    url(r'^user_agreement$', v.userAgreement, name='user_agreement'),
     url(r'^login$', v.login_view, name='login'),
     url(r'^home$', v.home_view, name='home'),
     url(r'', v.home_view, name='home'),    
