49739b40e8eb5a61206a99edf1044f2f7203575e
Darius Calliet <dcalliet@topoppssoftware.com>
2016-02-18 00:12:08 -0600

    adding 404 page

--- civiwiki/api/urls_templates.py
+++ civiwiki/api/urls_templates.py
@@ -4,6 +4,6 @@ import views_templates as v
 urlpatterns = [
     url(r'^createpage', v.create_page, name='add page'),
     url(r'^login$', v.login_view, name='login'),
-    url(r'^home$', v.home_view, name='home'),
+    url(r'^$', v.home_view, name='home'),
     url(r'', v.does_not_exist, name='404')
 ]
