af7c3f537065a44d25f3330ca5608f5ad6c09b8a
abctwinkie <cameronwong@users-MacBook-Air.local>
2016-03-19 12:17:35 -0500

    pics are coming soon

--- civiwiki/frontend/static/views/account_base_view.js
+++ civiwiki/frontend/static/views/account_base_view.js
@@ -35,17 +35,48 @@ var AccountBaseView = Backbone.View.extend({
     },
 
     getInterests: function(){
+        var _this = this;               
+        _this.$el.empty();
         console.log("heythere");
             this.interests.fetch({ 
             success: function (collection, response) {
                 this.interests = collection;
-                console.log(this.interests.length);
-                console.log(this);
+                //console.log(this.interests.length);
+                //console.log(this);
                 //debugger;
+               var counter = 1;
+               _this.$el.append("<table style='width:100%'>");
+               var percentage = (100/(this.interests.length/3));
+               for(var i = 0; i < this.interests.length; i=i+3){
+                var interest1 = this.interests.models[i].attributes.name.replace(/ /g,'');
+                var interest2 = this.interests.models[i+1].attributes.name.replace(/ /g,'');
+                var interest3 = this.interests.models[i+2].attributes.name.replace(/ /g,'');
+                var interest11 = "static/img/interest_pics/" + interest1 + ".jpg";
+                var interest22 = "static/img/interest_pics/" + interest2 + ".jpg";
+                var interest33 = "static/img/interest_pics/" + interest3 + ".jpg";
+                _this.$el.append('<tr>'+'<td width='+ percentage + '%><center>' + '<center>' + this.interests.models[i].attributes.name + '</center><br>'+"<img src="+interest11+" width = 100% width = autoheight = auto/>" + '</center>'+ '</td>' 
+                    +'<td width='+ percentage + '%><center>' +  '<center>' + this.interests.models[i+1].attributes.name + '</center><br>'+"<img src="+interest22+" width = 100% height = auto/>"  + '</center>'+ '</td>'
+                    +'<td width='+ percentage + '%><center>' +  '<center>' + this.interests.models[i+2].attributes.name + '</center><br>'+"<img src="+interest33+" width = 100% height = auto/>"  + '</center>'+ '</td><tr>');
 
-                _.each(this.interests.models,function(someThing){
+
+
+
+               }
+
+            /*_.each(this.interests.models,function(someThing){
                     console.log(someThing.attributes.name);
+                    if(counter%3 == 1){
+                        _this.$el.append('<tr><td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td>')
+                    }else if (counter%3 == 2){
+                        _this.$el.append('<td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td>')
+                    }else{
+                        _this.$el.append('<td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td></tr>')
+                    }
                 });
+            */
+               _this.$el.append("</table>");
+                           
+
             }
         });
         console.log("yolo");
@@ -60,7 +91,7 @@ var AccountBaseView = Backbone.View.extend({
         _this.$el.empty().append(_this.baseTemplate({
             user: _this.userModel.toJSON()
         }));
-
+        
     }
 });
 var temp =  new Backbone.Model();
