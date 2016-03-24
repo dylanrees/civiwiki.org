7a724288f9ddf5b96c63cac85d5dd08f02d7d864
abctwinkie <cameronwong@users-air.dhcp.wustl.edu>
2016-03-24 15:56:50 -0500

    still in progress

--- civiwiki/frontend/static/views/account_base_view.js
+++ civiwiki/frontend/static/views/account_base_view.js
@@ -8,10 +8,7 @@ var interest_views = Backbone.Collection.extend({
 
 });
 
-function fnChangeBorder(imageID){
-      var imgElement = document.getElementById(imageID);
-      imgElement.setAttribute("style:borderColor", "#00FF00");
-};
+
 
 var AccountBaseView = Backbone.View.extend({
 
@@ -19,6 +16,7 @@ var AccountBaseView = Backbone.View.extend({
 
 
     baseTemplate: _.template($('#base-template').html()),
+    interestTemplate: _.template($('#interest-tab-template').html()),
 
     initialize: function (options) {
         var _this = this;
@@ -26,7 +24,6 @@ var AccountBaseView = Backbone.View.extend({
 
         options = options || {};
         _this.userModel = options.userModel;
-
         _this.render();
         console.log("the view")
         console.log(this.View);
@@ -34,7 +31,6 @@ var AccountBaseView = Backbone.View.extend({
     },
 
     events: {
-        "click .tab.col.s3.interests": "getInterests"
 
     },
 
@@ -43,34 +39,7 @@ var AccountBaseView = Backbone.View.extend({
             this.interests.fetch({ 
             success: function (collection, response) {
                 this.interests = collection;
-                //console.log(this.interests.length);
-                //console.log(this);
-                //debugger;
-               _this.$el.append("<table style='width:100%' border = 1>");
-               var percentage = (100/(this.interests.length/3));
-               for(var i = 0; i < this.interests.length; i=i+3){
-                var interest1 = this.interests.models[i].attributes.name.replace(/ /g,'');
-                var interest2 = this.interests.models[i+1].attributes.name.replace(/ /g,'');
-                var interest3 = this.interests.models[i+2].attributes.name.replace(/ /g,'');
-                var interest11 = "static/img/interest_pics/" + interest1 + ".jpg";
-                var interest22 = "static/img/interest_pics/" + interest2 + ".jpg";  
-                var interest33 = "static/img/interest_pics/" + interest3 + ".jpg";
-                _this.$el.append('<tr>'+'<td width='+ percentage + '%><center>' + '<center>' + this.interests.models[i].attributes.name + '</center><br>'+"<img id = " +interest1 + " class='img-circle' src="+interest11+" width = 100% width = autoheight = auto/>" + '</center>'+ '</td>' 
-                    +'<td width='+ percentage + '%><center>' +  '<center>' + this.interests.models[i+1].attributes.name + '</center><br>'+"<img id = " +interest2 + " class='img-circle' src="+interest22+" width = 100% height = auto/>"  + '</center>'+ '</td>'
-                    +'<td width='+ percentage + '%><center>' +  '<center>' + this.interests.models[i+2].attributes.name + '</center><br>'+"<img id = " +interest3 + " class='img-circle' src="+interest33+" width = 100% height = auto/>"  + '</center>'+ '</td><tr>');
-               }
-            /*_.each(this.interests.models,function(someThing){
-                    console.log(someThing.attributes.name);
-                    if(counter%3 == 1){
-                        _this.$el.append('<tr><td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td>')
-                    }else if (counter%3 == 2){
-                        _this.$el.append('<td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td>')
-                    }else{
-                        _this.$el.append('<td><center>' + "<img src='static/img/interest_pics/Business&Economy.jpg' width = 20%/>" + '</center><br>'+ '<center>' + someThing.attributes.name + '</center></td></tr>')
-                    }
-                });
-            */
-               _this.$el.append("</table>");
+             
 
             }
         });
@@ -81,10 +50,11 @@ var AccountBaseView = Backbone.View.extend({
     render: function () {
 
         var _this = this;
-
         _this.$el.empty().append(_this.baseTemplate({
             user: _this.userModel.toJSON()
         }));
+        this.$el.find('#interests').empty().append(this.interest-tab-template({}));
+
         
     }
 });
