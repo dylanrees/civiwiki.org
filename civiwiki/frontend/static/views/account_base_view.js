68add32320d5ebb59a35b84ab6297100156971d1
abctwinkie <cameronwong@users-air.dhcp.wustl.edu>
2016-03-30 13:30:59 -0500

    zzz

--- civiwiki/frontend/static/views/account_base_view.js
+++ civiwiki/frontend/static/views/account_base_view.js
@@ -53,33 +53,35 @@ var AccountBaseView = Backbone.View.extend({
 
     editFollowInterest: function(event){
        var _this = this;
-       var tempInterest = _this.userModel.get("interests");
+       var tempInterest = JSON.parse(_this.userModel.get("interests"));
        var follow = true;
        var unfollow = tempInterest.indexOf(event.target.id);
        var temp1 = event.target.id;
-       console.log(temp1);
        var temp = temp1.replace(/ /g,'');
        console.log("Before: " +tempInterest);
 
        if (unfollow == -1){
             tempInterest.push(event.target.id);
             document.getElementById(event.target.id).innerHTML = "Click Here to Unfollow";
-            document.getElementById(temp).innerHTML = "Click Here to Unfollow";
+            //document.getElementById(temp).innerHTML = "Click Here to Unfollow";
        }else{
             tempInterest.splice(unfollow, 1);
             document.getElementById(event.target.id).innerHTML = "Click Here to Follow";
-            document.getElementById(temp).innerHTML = "Click Here to Follow";
+            //document.getElementById(temp).innerHTML = "Click Here to Follow";
        }
        console.log("After: " +tempInterest);
        $.ajax({
             type: 'POST',
             url: '/api/edituser',
             data: {
-                data : JSON.stringify({interests: tempInterest})
+                //data : JSON.stringify({interests: tempInterest})
+                interests: JSON.stringify(tempInterest)
             },
             success: function (data) {
+                console.log("yes");
             },
             error: function(data){
+                console.log("no");                
             }
         });
 
