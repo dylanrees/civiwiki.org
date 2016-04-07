2ca929e8e90f72a7b6ec2434a52b8da88435ede9
Darius Calliet <dcalliet@topoppssoftware.com>
2016-04-06 21:33:21 -0500

    merge master

--- civiwiki/frontend/static/views/account_base_view.js
+++ civiwiki/frontend/static/views/account_base_view.js
@@ -10,13 +10,12 @@ var AccountBaseView = Backbone.View.extend({
         options = options || {};
         _this.userModel = options.userModel;
         _this.render();
-
         this.friendsTab = new FriendsListView({
             userModel : _this.userModel,
             friends: _this.userModel.toJSON().friend_requests,
-            user_id: _this.userModel.toJSON().user_id, 
+            user_id: _this.userModel.toJSON().user_id,
         });
-        
+
         this.friendRequestView = new FriendRequestView({
             friend_requests: _this.userModel.toJSON().friend_requests
         });
@@ -28,7 +27,7 @@ var AccountBaseView = Backbone.View.extend({
         var _this = this;
 
         _this.$el.empty().append(_this.baseTemplate({
-            user: _this.userModel.toJSON(), 
+            user: _this.userModel.toJSON(),
         }));
 
     },
