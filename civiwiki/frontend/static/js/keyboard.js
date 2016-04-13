90c1ab9a8481e15ec7a99af61fa702b272a9626c
LawranceFung <fungjl@wustl.edu>
2016-04-13 18:02:45 -0500

    basic shortcuts written, but not yet attached

---
+++ civiwiki/frontend/static/js/keyboard.js
@@ -0,0 +1,8 @@
+// Go homepage
+Mousetrap.bind('h', function() { window.location.href="/"; });
+// Go to account page
+Mousetrap.bind('a', function() { window.location.href="/account"; });
+// About page
+Mousetrap.bind('b', function() { window.location.href="/about"; });
+// Support page
+Mousetrap.bind('s', function() { window.location.href="/supportus"; });
