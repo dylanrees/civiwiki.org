3f937c7a878d02cfb2a436e2cd5110faee4b372c
Darius Calliet <dcalliet@topoppssoftware.com>
2016-04-14 11:23:27 -0500

    adding default stuff to the db info

--- civiwiki/utils/db_info.py
+++ civiwiki/utils/db_info.py
@@ -1,1 +1,10 @@
-
+DATABASES = {
+    'default': {
+        'HOST':'database_host',
+        'PORT': 'database_port',
+        'NAME': 'database_name',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'database_user',
+        'PASSWORD': 'database_pass',
+    },
+}
